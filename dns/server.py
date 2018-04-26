586654c35960b6309050fa36f48c1527619160f4
unh0lys0daz <unh0lys0daz@gmail.com>
2018-04-26 16:38:54 +0200

    Bla

--- dns/server.py
+++ dns/server.py
@@ -8,19 +8,31 @@ server using the algorithm described in section 4.3.2 of RFC 1034.
 
 
 from threading import Thread
-
+from dns.message import Message
+from dns.zone import Zone
+from dns.zone import Catalog
 
 class RequestHandler(Thread):
     """A handler for requests to the DNS server"""
 
-    def __init__(self):
+    def __init__(self, data, addr):
         """Initialize the handler thread"""
         super().__init__()
         self.daemon = True
+        self.data = data
+        self.addr = addr
 
     def run(self):
         """ Run the handler thread"""
-        pass
+        msg = Message.from_bytes(self.data)
+        header = msg.header
+        recursion = header.rd() != 0
+        questions = msg.questions
+        for question in questions:
+
+
+
+
 
 
 class Server:
@@ -41,8 +53,17 @@ class Server:
 
     def serve(self):
         """Start serving requests"""
+        zone = Zone()
+        zone.read_master_file('zone')
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.bind(('127.0.0.1',self.port))
+        sock.listen(10)
         while not self.done:
-            pass
+            data, addr = sock.recvfrom(65000) 
+            handler = RequestHandler(data, addr, zone)
+            handler.run()
+
+            
 
     def shutdown(self):
         """Shut the server down"""
